e2e39655b0017af1bebc70b37493b4e58a691d3b
LEARN Academy <jeffreysjoshua@gmail.com>
2021-12-29 20:52:17 -0800

    Light bulb and switch challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
 import Button from './components/Button.js'
+import Bulb from './components/Bulb.js'
 
 class App extends Component{
   render(){
     return(
       <>
         <h1>Hello Echo!</h1>
-        <h2> Josh's Lightbulb Challenge!</h2>
-        <Button/>
+        <h2> Josh's Lightbulb Challenge</h2>
+        <Bulb/>
       </>
     )
   }
